f02feb168b587a97eb00e8216e9f19b4b4127a41
rishabh <rishabh.dixit123@gmail.com>
2014-06-30 19:07:45 +0530

    Exercise

--- Exercise/box.js
+++ Exercise/box.js
@@ -1,5 +1,43 @@
-/**
- * Created by amit on 18/6/14.
- */
+
 "use strict";
 
+
+var check = (function() {
+    return function check(obj) {
+        if (obj.style.backgroundColor != "red" && obj.style.backgroundColor != "blue" && obj.style.backgroundColor != "green") {
+            var button = document.getElementsByName("color");
+
+            var value;
+            console.log(button.length);
+            for (var i = 0; i < button.length; i++) {
+                if (button[i].checked) {
+                    value = button[i].value;
+                    break;
+
+                }
+            }
+            if (value != undefined) {
+                obj.style.backgroundColor = value;
+
+            }
+            else alert("please first select a color");
+
+        }
+        else
+            alert("color can not be set again because its color is :" + obj.style.backgroundColor);
+    }
+})();
+
+var reset = (function(){
+
+    return function reset() {
+        var arr = document.getElementsByClassName("number");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].style.backgroundColor = "";
+        }
+        var arr1 = document.getElementsByName("color");
+        for (var i = 0; i < arr1.length; i++) {
+            arr1[i].checked = false;
+        }
+    }
+})();
